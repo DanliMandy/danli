f816376f113016357c3da22471cfb8086c6693a7
DanliMandy <435192837@qq.com>
2018-05-07 10:57:55 +0800

    提交

---
+++ firstproject/login.js
@@ -0,0 +1,13 @@
+var user = "admin"
+var psd = "123"
+
+$("#login").click(function () {
+    layer.open({
+        type: 1,
+        title: false,
+        closeBtn: 0,
+        shadeClose: true,
+        skin: 'yourclass',
+        content: "fshfwho"
+    });
+})
