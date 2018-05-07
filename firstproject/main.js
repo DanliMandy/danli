e2e6237d3443c2f3d66e301bb9fe057f42f58165
DanliMandy <435192837@qq.com>
2018-05-07 09:13:09 +0800

    提交

--- firstproject/main.js
+++ firstproject/main.js
@@ -1,3 +1,19 @@
+var question = "放手工活公检法评价";
+var words = "小水皮大丹丽呵嘿哈";
+var rightword = "小水皮大" ;
+//打乱words
+function upsetArr(arr){
+    return arr.sort(function(){ return Math.random() - 0.5});
+}
+var newWords = upsetArr(words.split("")).join("");
+//原理很简单，就是把字符串转成数组，打乱后，再转成字符串
+$("#question").text(question);
+//填入words
+for (var i = 0;i< newWords.length; i++){
+    $(".words").children().eq(i).text(newWords[i]);
+}
+
+
 //下面的字点击事件
 $(".word").click(function () {
     if ($(".cell").text().length == $(".cells").children().length){
@@ -14,13 +30,27 @@ $(".word").click(function () {
         $(this).text("");
         frame.attr("wordId",$(this).attr("id"));
     }
-    while ($(".cell").text().length == $(".cells").children().length) {
-        if ($(".cell").text() == "小水皮大") {
-            alert("right")
 
+    while ($(".cell").text().length == $(".cells").children().length) {
+        if ($(".cell").text() == rightword) {
+            setTimeout(function () {
+                layer.msg('正确');
+            }, 400)
         }
         else {
-            alert("wrong")
+            for(var j = 0;j < $(".cell").length; j++){
+                var cellid = $(".cells").children().eq(j);
+                for (var k = 0;k < $(".word").length; k++) {
+                    var wordid = $(".words").children().eq(k);
+                    if (cellid.attr("wordId") == wordid.attr("id")) {
+                        wordid.text(cellid.text());
+                        cellid.text("");
+                    }
+                }
+            }
+            setTimeout(function () {
+                layer.msg('错误');
+            }, 400)
         }
         return
     }
