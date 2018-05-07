9dfc3a0e30bb3a64a9357aba11fcc6457b16d460
bobo <393841724@qq,com>
2018-05-07 12:01:11 +0800

    更新登录

--- firstproject/login.js
+++ firstproject/login.js
@@ -1,13 +1,2 @@
 var user = "admin"
 var psd = "123"
-
-$("#login").click(function () {
-    layer.open({
-        type: 1,
-        title: "登录",
-        closeBtn: 0,
-        shadeClose: true,
-        skin: 'yourclass',
-        content: "fshfwho"
-    });
-})
